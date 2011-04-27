9201f9f461f29f9166f67e0d7c626eff714c2b17
Doug Johnson doug.johnson@sage.com <doug.johnson@sage.com>
2011-04-28 00:48:07 +0100

    added outer_window to pagination

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,6 @@
 class TicketsController < ApplicationController
   def index
-    @tickets = Ticket.order("id desc").page(params[:page]).per(50)
+    @tickets = Ticket.order("id desc").page(params[:page]).per(30)
     
     respond_to do |format|
       format.html

7c31b62db8b0ed86d9e200d8d36e486bdd74d6ec
Doug Johnson doug.johnson@sage.com <doug.johnson@sage.com>
2011-06-18 22:56:32 +0100

    added heroku callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-   provider :github, 'd191ee3944195bbe74eb', '2720335c5c74831c5acfc1044c3eab36e26adeef'  
+  if RAILS_ENV == 'production' then
+    provider :github, '4ffcf667d5aeaa52ac7a', 'a0968652c83e4e89dd5062b956b7011f3c6c00cf'
+  else
+    provider :github, 'd191ee3944195bbe74eb', '2720335c5c74831c5acfc1044c3eab36e26adeef'
+  end
 end 
